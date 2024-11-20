2bd4c7c28090c22a45908ee6ef56bc5112dbfb0f
00JIMMY00 <jimmygamaljimmy@gmail.com>
2024-11-21 01:54:56 +0200

    insert-retrive program

--- VB_SQL_App/Form1.Designer.vb
+++ VB_SQL_App/Form1.Designer.vb
@@ -22,28 +22,75 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.btnInsert = New System.Windows.Forms.Button()
+        Me.btnRetrieve = New System.Windows.Forms.Button()
+        Me.dgvUsers = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvUsers, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'txtUsername
         '
-        Me.TextBox1.Location = New System.Drawing.Point(196, 110)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(198, 20)
-        Me.TextBox1.TabIndex = 0
+        Me.txtUsername.Location = New System.Drawing.Point(84, 42)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(198, 20)
+        Me.txtUsername.TabIndex = 0
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(84, 79)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(198, 20)
+        Me.txtEmail.TabIndex = 1
+        '
+        'btnInsert
+        '
+        Me.btnInsert.Location = New System.Drawing.Point(84, 126)
+        Me.btnInsert.Name = "btnInsert"
+        Me.btnInsert.Size = New System.Drawing.Size(75, 23)
+        Me.btnInsert.TabIndex = 2
+        Me.btnInsert.Text = "Insert User"
+        Me.btnInsert.UseVisualStyleBackColor = True
+        '
+        'btnRetrieve
+        '
+        Me.btnRetrieve.Location = New System.Drawing.Point(207, 126)
+        Me.btnRetrieve.Name = "btnRetrieve"
+        Me.btnRetrieve.Size = New System.Drawing.Size(75, 23)
+        Me.btnRetrieve.TabIndex = 3
+        Me.btnRetrieve.Text = "Retrieve Users"
+        Me.btnRetrieve.UseVisualStyleBackColor = True
+        '
+        'dgvUsers
+        '
+        Me.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvUsers.Location = New System.Drawing.Point(84, 174)
+        Me.dgvUsers.Name = "dgvUsers"
+        Me.dgvUsers.Size = New System.Drawing.Size(198, 327)
+        Me.dgvUsers.TabIndex = 4
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(829, 464)
-        Me.Controls.Add(Me.TextBox1)
+        Me.ClientSize = New System.Drawing.Size(829, 526)
+        Me.Controls.Add(Me.dgvUsers)
+        Me.Controls.Add(Me.btnRetrieve)
+        Me.Controls.Add(Me.btnInsert)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtUsername)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.dgvUsers, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents btnInsert As Button
+    Friend WithEvents btnRetrieve As Button
+    Friend WithEvents dgvUsers As DataGridView
 End Class
--- VB_SQL_App/Form1.vb
+++ VB_SQL_App/Form1.vb
@@ -1,3 +1,57 @@
-﻿Public Class Form1
+﻿
+Imports System.Data.SqlClient
+
+Public Class Form1
+
+    ' Define the connection string (replace YOUR_SERVER_NAME with your server name)
+    Private connectionString As String = "Data Source=DESKTOP-DBR7VNK\SQLEXPRESS;Initial Catalog=TestDB;Integrated Security=True"
+
+    ' Function to insert a user into the database
+    Private Sub InsertUser(username As String, email As String)
+        Using con As New SqlConnection(connectionString)
+            Dim query As String = "INSERT INTO Users (Username, Email) VALUES (@Username, @Email)"
+            Using cmd As New SqlCommand(query, con)
+                ' Add parameters to prevent SQL injection
+                cmd.Parameters.AddWithValue("@Username", username)
+                cmd.Parameters.AddWithValue("@Email", email)
+                con.Open()
+                cmd.ExecuteNonQuery()
+                MessageBox.Show("User inserted successfully.")
+            End Using
+        End Using
+    End Sub
+
+    ' Function to retrieve users from the database
+    Private Sub RetrieveUsers()
+        Using con As New SqlConnection(connectionString)
+            Dim query As String = "SELECT * FROM Users"
+            Using cmd As New SqlCommand(query, con)
+                con.Open()
+                Dim dt As New DataTable()
+                dt.Load(cmd.ExecuteReader())
+                ' Bind the DataTable to the DataGridView
+                dgvUsers.DataSource = dt
+            End Using
+        End Using
+    End Sub
+
+    ' Event handler for the Insert button click
+    Private Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
+        Dim username As String = txtUsername.Text.Trim()
+        Dim email As String = txtEmail.Text.Trim()
+
+        ' Simple validation to ensure fields are not empty
+        If String.IsNullOrEmpty(username) OrElse String.IsNullOrEmpty(email) Then
+            MessageBox.Show("Please enter both username and email.")
+            Return
+        End If
+
+        InsertUser(username, email)
+    End Sub
+
+    ' Event handler for the Retrieve button click
+    Private Sub btnRetrieve_Click(sender As Object, e As EventArgs) Handles btnRetrieve.Click
+        RetrieveUsers()
+    End Sub
 
 End Class
