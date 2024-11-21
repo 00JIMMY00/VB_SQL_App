273f062eb98fcf8dc05f2d923c1e98a50843595c
00JIMMY00 <jimmygamaljimmy@gmail.com>
2024-11-21 02:44:47 +0200

    3 tables m2m relation , choose userid from ui

--- VB_SQL_App/Form1.Designer.vb
+++ VB_SQL_App/Form1.Designer.vb
@@ -27,7 +27,16 @@ Partial Class Form1
         Me.btnInsert = New System.Windows.Forms.Button()
         Me.btnRetrieve = New System.Windows.Forms.Button()
         Me.dgvUsers = New System.Windows.Forms.DataGridView()
+        Me.cmbProducts = New System.Windows.Forms.ComboBox()
+        Me.txtQuantity = New System.Windows.Forms.TextBox()
+        Me.btnPurchase = New System.Windows.Forms.Button()
+        Me.dgvPurchases = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
         CType(Me.dgvUsers, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvPurchases, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'txtUsername
@@ -65,16 +74,96 @@ Partial Class Form1
         'dgvUsers
         '
         Me.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvUsers.Location = New System.Drawing.Point(84, 174)
+        Me.dgvUsers.Location = New System.Drawing.Point(25, 171)
         Me.dgvUsers.Name = "dgvUsers"
-        Me.dgvUsers.Size = New System.Drawing.Size(198, 327)
+        Me.dgvUsers.Size = New System.Drawing.Size(347, 327)
         Me.dgvUsers.TabIndex = 4
+        '
+        'cmbProducts
+        '
+        Me.cmbProducts.FormattingEnabled = True
+        Me.cmbProducts.Location = New System.Drawing.Point(480, 74)
+        Me.cmbProducts.Name = "cmbProducts"
+        Me.cmbProducts.Size = New System.Drawing.Size(121, 21)
+        Me.cmbProducts.TabIndex = 5
+        '
+        'txtQuantity
+        '
+        Me.txtQuantity.Location = New System.Drawing.Point(620, 74)
+        Me.txtQuantity.Name = "txtQuantity"
+        Me.txtQuantity.Size = New System.Drawing.Size(100, 20)
+        Me.txtQuantity.TabIndex = 6
+        '
+        'btnPurchase
+        '
+        Me.btnPurchase.Location = New System.Drawing.Point(480, 113)
+        Me.btnPurchase.Name = "btnPurchase"
+        Me.btnPurchase.Size = New System.Drawing.Size(240, 23)
+        Me.btnPurchase.TabIndex = 7
+        Me.btnPurchase.Text = "Purchase Product"
+        Me.btnPurchase.UseVisualStyleBackColor = True
+        '
+        'dgvPurchases
+        '
+        Me.dgvPurchases.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvPurchases.Location = New System.Drawing.Point(480, 171)
+        Me.dgvPurchases.Name = "dgvPurchases"
+        Me.dgvPurchases.Size = New System.Drawing.Size(301, 150)
+        Me.dgvPurchases.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(519, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 18)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Product"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(633, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 18)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Quantity"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(26, 41)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 18)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(22, 76)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(56, 18)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "E-mail"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(829, 526)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dgvPurchases)
+        Me.Controls.Add(Me.btnPurchase)
+        Me.Controls.Add(Me.txtQuantity)
+        Me.Controls.Add(Me.cmbProducts)
         Me.Controls.Add(Me.dgvUsers)
         Me.Controls.Add(Me.btnRetrieve)
         Me.Controls.Add(Me.btnInsert)
@@ -83,6 +172,7 @@ Partial Class Form1
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.dgvUsers, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvPurchases, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -93,4 +183,12 @@ Partial Class Form1
     Friend WithEvents btnInsert As Button
     Friend WithEvents btnRetrieve As Button
     Friend WithEvents dgvUsers As DataGridView
+    Friend WithEvents cmbProducts As ComboBox
+    Friend WithEvents txtQuantity As TextBox
+    Friend WithEvents btnPurchase As Button
+    Friend WithEvents dgvPurchases As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 End Class
--- VB_SQL_App/Form1.vb
+++ VB_SQL_App/Form1.vb
@@ -1,13 +1,23 @@
-﻿
-Imports System.Data.SqlClient
+﻿Imports System.Data.SqlClient
 
 Public Class Form1
 
     ' Define the connection string (replace YOUR_SERVER_NAME with your server name)
     Private connectionString As String = "Data Source=DESKTOP-DBR7VNK\SQLEXPRESS;Initial Catalog=TestDB;Integrated Security=True"
 
+    ' Variable to store the current selected user ID
+    Private currentUserID As Integer = -1
+    Private txtSelectedUser As Object
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Load products into the ComboBox when the form loads
+        LoadProducts()
+        ' Retrieve users to display in dgvUsers
+        RetrieveUsers()
+    End Sub
+
     ' Function to insert a user into the database
-    Private Sub InsertUser(username As String, email As String)
+    Public Sub InsertUser(username As String, email As String)
         Using con As New SqlConnection(connectionString)
             Dim query As String = "INSERT INTO Users (Username, Email) VALUES (@Username, @Email)"
             Using cmd As New SqlCommand(query, con)
@@ -17,20 +27,72 @@ Public Class Form1
                 con.Open()
                 cmd.ExecuteNonQuery()
                 MessageBox.Show("User inserted successfully.")
+                ' Refresh the users list
+                RetrieveUsers()
             End Using
         End Using
     End Sub
 
     ' Function to retrieve users from the database
-    Private Sub RetrieveUsers()
+    Public Sub RetrieveUsers()
         Using con As New SqlConnection(connectionString)
             Dim query As String = "SELECT * FROM Users"
             Using cmd As New SqlCommand(query, con)
                 con.Open()
                 Dim dt As New DataTable()
                 dt.Load(cmd.ExecuteReader())
-                ' Bind the DataTable to the DataGridView
                 dgvUsers.DataSource = dt
+            End Using
+        End Using
+    End Sub
+
+    ' Function to load products into the ComboBox
+    Private Sub LoadProducts()
+        Using con As New SqlConnection(connectionString)
+            Dim query As String = "SELECT ProductID, ProductName FROM Products"
+            Using cmd As New SqlCommand(query, con)
+                con.Open()
+                Dim dt As New DataTable()
+                dt.Load(cmd.ExecuteReader())
+                cmbProducts.DataSource = dt
+                cmbProducts.DisplayMember = "ProductName"
+                cmbProducts.ValueMember = "ProductID"
+            End Using
+        End Using
+    End Sub
+
+    ' Function to purchase a product
+    Public Sub PurchaseProduct(userID As Integer, productID As Integer, quantity As Integer)
+        Using con As New SqlConnection(connectionString)
+            Dim query As String = "
+                INSERT INTO UserProducts (UserID, ProductID, Quantity)
+                VALUES (@UserID, @ProductID, @Quantity)"
+            Using cmd As New SqlCommand(query, con)
+                cmd.Parameters.AddWithValue("@UserID", userID)
+                cmd.Parameters.AddWithValue("@ProductID", productID)
+                cmd.Parameters.AddWithValue("@Quantity", quantity)
+                con.Open()
+                cmd.ExecuteNonQuery()
+                MessageBox.Show("Product purchased successfully.")
+            End Using
+        End Using
+    End Sub
+
+    ' Function to retrieve purchased products for a user
+    Private Sub RetrievePurchases(userID As Integer)
+        Using con As New SqlConnection(connectionString)
+            Dim query As String = "
+                SELECT UP.UserProductID, P.ProductName, UP.Quantity, UP.PurchaseDate
+                FROM UserProducts UP
+                INNER JOIN Products P ON UP.ProductID = P.ProductID
+                WHERE UP.UserID = @UserID
+                ORDER BY UP.PurchaseDate DESC"
+            Using cmd As New SqlCommand(query, con)
+                cmd.Parameters.AddWithValue("@UserID", userID)
+                con.Open()
+                Dim dt As New DataTable()
+                dt.Load(cmd.ExecuteReader())
+                dgvPurchases.DataSource = dt
             End Using
         End Using
     End Sub
@@ -47,11 +109,45 @@ Public Class Form1
         End If
 
         InsertUser(username, email)
+        ' Clear input fields after insertion
+        txtUsername.Clear()
+        txtEmail.Clear()
     End Sub
 
     ' Event handler for the Retrieve button click
     Private Sub btnRetrieve_Click(sender As Object, e As EventArgs) Handles btnRetrieve.Click
         RetrieveUsers()
+    End Sub
+
+    ' Event handler for the Purchase button click
+    Private Sub btnPurchase_Click(sender As Object, e As EventArgs) Handles btnPurchase.Click
+        If currentUserID = -1 Then
+            MessageBox.Show("Please select a user first.")
+            Return
+        End If
+
+        Dim productID As Integer = CInt(cmbProducts.SelectedValue)
+        Dim quantity As Integer
+
+        If Not Integer.TryParse(txtQuantity.Text.Trim(), quantity) OrElse quantity <= 0 Then
+            MessageBox.Show("Please enter a valid quantity.")
+            Return
+        End If
+
+        PurchaseProduct(currentUserID, productID, quantity)
+        RetrievePurchases(currentUserID) ' Refresh the purchases DataGridView
+    End Sub
+
+    ' Event handler for selecting a user from dgvUsers
+    Private Sub dgvUsers_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvUsers.CellClick
+        ' Get the selected user's ID
+        If e.RowIndex >= 0 Then
+            Dim row As DataGridViewRow = dgvUsers.Rows(e.RowIndex)
+            currentUserID = CInt(row.Cells("UserID").Value)
+            txtSelectedUser.Text = row.Cells("Username").Value.ToString()
+            ' Retrieve purchases for the selected user
+            RetrievePurchases(currentUserID)
+        End If
     End Sub
 
 End Class
